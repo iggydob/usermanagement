3cb5c85b14909fd8383fc490e178bc975b628aa2
iggydob <138367360+iggydob@users.noreply.github.com>
2024-07-03 00:54:32 +0300

    Application.yml configuration fixed; Added hibernate-orm dependency in build.gradle; User.java updated;

--- src/main/java/bg/energopro/domain/User.java
+++ src/main/java/bg/energopro/domain/User.java
@@ -15,7 +15,7 @@ import java.time.LocalDateTime;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "user_id")
     public Long userId;
 
